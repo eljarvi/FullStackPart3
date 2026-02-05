a554fe11200f21692b6507e6fd87c224ac350600
Ellen <jarvi.ellen@gmail.com>
2026-02-05 17:12:49 +0200

    Exercise 3.22

--- puhelinluettelo/backend/models/person.js
+++ puhelinluettelo/backend/models/person.js
@@ -6,31 +6,31 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3
-    },
-    number: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  number: {
+    type: String,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- puhelinluettelo/backend/mongo.js
+++ puhelinluettelo/backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length !== 3 && process.argv.length !== 5) {
-    console.log('give password, or password name number')
-    process.exit(1)
+  console.log('give password, or password name number')
+  process.exit(1)
 }
 const password = process.argv[2]
 
@@ -12,34 +12,34 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    console.log('phonebook:')
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
     })
+    mongoose.connection.close()
+  })
 
 } else {
-    const personName = process.argv[3]
-    const personNumber = process.argv[4]
+  const personName = process.argv[3]
+  const personNumber = process.argv[4]
 
 
-    const person = new Person({
-        name: personName,
-        number: personNumber
-    })
+  const person = new Person({
+    name: personName,
+    number: personNumber
+  })
 
-    person.save().then(result => {
-        console.log(`added ${personName} number ${personNumber} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(result => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
